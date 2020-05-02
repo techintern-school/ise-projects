ae7c41bfa4a1b2b5feb35f9b3febd5e4e0b22638
“Geoff <geoff616@gmail.com>
2020-05-02 08:59:07 -1000

    refactor project 1 and add challenge 1

--- project-1/challenge-1/check_solution.js
+++ project-1/challenge-1/check_solution.js
@@ -2,8 +2,17 @@
 
 const utils = require("../../utils.js");
 const p1 = require("../p1.js");
-//const { execSync } = require("child_process");
-//const result = execSync("python solution.py --brand nike --discount 20");
+var fs = require("fs");
+var dir = "./tmp";
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+const { execSync } = require("child_process");
+// test the script using the brand and discount from challenge 1
+// TODO: enumerate more test cases
+// TODO: abstract out the logic to re-use in multiple test cases
+execSync("python solution.py --brand nike --discount 20");
 utils
   .getTwoCSVFiles(
     `${__dirname}/product_catalog_updated.csv`,
--- project-1/challenge-1/solution.py
+++ project-1/challenge-1/solution.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--brand')
-parser.add_argument('--discount')
-args = parser.parse_args()
-print(args.brand)
+
+# TODO implement your solution here
